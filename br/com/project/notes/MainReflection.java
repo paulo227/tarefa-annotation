56c025dac0e3b79bcadcd3361a2d664dc40855db
paulo felipe <103428918+paulo227@users.noreply.github.com>
2023-10-09 12:25:21 -0300

    Segundo commit

---
+++ br/com/project/notes/MainReflection.java
@@ -0,0 +1,13 @@
+package br.com.project.notes;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) {
+        Class clazz = ClasseComAnotacao.class;
+        System.out.println(clazz);
+        
+
+    }
+}
