617cd40fd7541e24a11c8db6459b373068c87922
paulo felipe <103428918+paulo227@users.noreply.github.com>
2024-02-14 15:53:07 -0300

    Segundo commit

--- br/com/project/notes/MainReflection.java
+++ br/com/project/notes/MainReflection.java
@@ -1,13 +1,44 @@
 package br.com.project.notes;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class MainReflection {
     public static void main(String[] args) {
-        Class clazz = ClasseComAnotacao.class;
-        System.out.println(clazz);
-        
+
+        try {
+            String classe = "ClasseAnnotation";
+            Class<?> ClasseComAnotacao = Class.forName("br.com.project.notes.ClasseComAnotacao"); // carrega
+            // classe
+            Object obj = ClasseComAnotacao.newInstance();
+            Field listaCampo[] = ClasseComAnotacao.getDeclaredFields(); // array que pega os
+            // campos
+
+            for (Field fld : listaCampo) {
+                Annotation[] annotations = fld.getAnnotations();
+
+                for (Annotation a : annotations) {
+
+                    System.out.println("Annotation utilizada: " + a);
+
+                    for (Method m : a.annotationType().getDeclaredMethods()) {
+                        System.out.println();
+
+                    }
+
+
+                }
+
+            }
+
+        } catch (Throwable e) {
+
+            System.err.println(e);
+
+        }
+
+
 
     }
 }
